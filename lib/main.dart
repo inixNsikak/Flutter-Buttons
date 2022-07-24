5bec3ee463b8c2d45d155ddffd6e870faf9fa3b8
inixNsikak <inixnsikak@gmail.com>
2022-07-24 21:20:34 +0100

    Flutter Buttons

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(FlutterButtons());
+
+
+class  FlutterButtons extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FlutterButtons"),
+        
+      ),
+      body: Center(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RaisedButton(
+            color: Colors.blue,
+            textColor:Colors.white,
+            child: Text('A Raised Button'),
+            onPressed:(){
+              print('Pressed raised button');
+            },
+            ),
+            FlatButton(
+              child:Text("A Flat button"),
+              textColor: Colors.blue,
+               onPressed: (){
+                print("Pressed a Flat button");
+               },
+               ),
+               OutlineButton(child: Text("An Outline button"),
+               textColor: Colors.blue, 
+               onPressed: (){
+                print("Pressed a Outllne button");
+               },
+               )
+        ],
+        ),
+      ),   
+
+    );
+  }
+}
